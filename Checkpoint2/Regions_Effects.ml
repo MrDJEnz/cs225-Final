272df298f162ef3978f7bce2f42961bfea8ae400
Duncan Enzmann <djenzmann@gmail.com>
2018-04-24 16:24:54 -0400

    Created the Checkpoint2 folder with the .ml file

---
+++ Checkpoint2/Regions_Effects.ml
@@ -0,0 +1,12 @@
+(* Names: Andy, Devin, Duncan, Jacob *)
+(* CS225 Final Project *)
+(* The following file is test cases to run our program... *)
+(* TODO all the draft test cases for now... *)
+
+
+
+
+
+
+
+(* Names: Andy, Devin, Duncan, Jacob*)
